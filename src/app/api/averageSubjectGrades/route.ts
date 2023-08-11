fbeba39f2ad094fdc36d7d7aab699c9b7865cc6b
Dylan <dylan.reeder5@gmail.com>
2023-08-11 09:46:04 -0400

    basic home bar chart

---
+++ src/app/api/averageSubjectGrades/route.ts
@@ -0,0 +1,25 @@
+import {NextResponse} from 'next/server';
+import {prisma} from '@/lib/db';
+
+export async function GET(req: Request) {
+  try {
+    const averages = await prisma.acedemics.groupBy({
+      by: ['subject'],
+      _avg: {
+        score: true,
+      },
+    });
+
+    const formattedData = averages.map((item) => ({
+      name: item.subject,
+      total: item._avg.score ? parseFloat(item._avg.score.toFixed(3)) : 0, // or any other default value you'd like
+    }));
+
+    return NextResponse.json(formattedData);
+  } catch (error) {
+    return NextResponse.json(
+      {error: 'Error fetching average scores'},
+      {status: 500}
+    );
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,64 @@
 
 import {Button} from '@/components/ui/button';
 import Link from 'next/link';
+import {useEffect, useState} from 'react';
+import {Bar, BarChart, ResponsiveContainer, XAxis, YAxis} from 'recharts';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | unknown>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/averageSubjectGrades');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {JSON.stringify(error)}</div>;
   return (
     <main className='max-w-3xl mx-auto'>
       <Link href='/grade'>
         <Button>Submit a Grade</Button>
       </Link>
+
+      <ResponsiveContainer width='100%' height={350}>
+        <BarChart data={data}>
+          <XAxis
+            dataKey='name'
+            stroke='#888888'
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+          />
+          <YAxis
+            stroke='#888888'
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={(value) => `${value}%`}
+          />
+          <Bar
+            dataKey='total'
+            fill='[#ffedd4, #ffb9a2]'
+            radius={[4, 4, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </main>
   );
 }
